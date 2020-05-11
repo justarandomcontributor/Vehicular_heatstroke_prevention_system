3140075684efa9d024727dfca9afd5bd027cc62c
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2020-05-11 14:53:52 +0530

    Update HeatStroke_prevention.ino

--- HeatStroke_prevention.ino
+++ HeatStroke_prevention.ino
@@ -1,3 +1,30 @@
+/*
+	Vehicular Heat Stroke Prevention System
+   
+   It's a prevention system in cars which trigger the parents through alarm, if the car temperature is beyond the given threshold and save the child
+   which is inside the car from heatstroke.
+	
+   Components used:
+   
+                                DHT-11 :-     digital pin 10
+   echo pin of ultrasonic sound sensor :-     digital pin 6
+   trigger pin of ultrasonic sound sensor :-  digital pin 4
+
+	Requirements:
+      + DHT library of arduino
+      + DHT11 temperature and humidity sensor
+      + Ultrasonic Distance sensor
+      + Buzzer for alarm
+
+	Created december 2019
+	By Harsh Sinha & Sushil Kumar Singh
+
+	https://github.com/justarandomcontributor/Vehicular_heatstroke_prevention_system
+
+*/
+
+// Source Code
+
 #include <dht.h>
 #define DHT11_PIN 10
 
@@ -7,6 +34,7 @@ const int pingPin = 4; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
 const int soundPin = 8;   //buzzer pin
 const int resetPin = 9;
+
 
 void setup()
 {
@@ -21,13 +49,15 @@ void setup()
    Serial.begin(9600);
    digitalWrite(12,0);
 }
+
 int previous=0;
+
 
 void sound_alarm()      //activates the alarm
 {
   int x=100;
-  while(1)      //infinite loop.
-  {
+   
+  while(1) {
     digitalWrite(8,1);    //buzzer activated
     delay(x);
     digitalWrite(8,0);
@@ -39,7 +69,8 @@ void sound_alarm()      //activates the alarm
   }
 }
 
-void check() //for checking the presence of a person.
+//for checking the presence of a person
+void check() 
 {
    int start_time=millis();
    long duration,cm;
@@ -49,40 +80,67 @@ void check() //for checking the presence of a person.
    delayMicroseconds(10);
    digitalWrite(pingPin, LOW);
    duration = pulseIn(echoPin, HIGH);
-   cm = microsecondsToCentimeters(duration);    //gets the distance in cm from obstacle using data obtained from distance sensor.
+   cm = microsecondsToCentimeters(duration); //gets the distance in cm from obstacle using data obtained from distance sensor
    Serial.println(cm);
-   if(millis()<500)   //distance sensor data collection variable initialization condition.
-   {
-    previous=cm;
-   }
-   if(previous-cm>.2)   //if current distance has changed changed or not
-   {
+   
+   //distance sensor data collection variable initialization condition.
+   if( millis() < 500){
+    previous=cm;}
+   
+   //if current distance has changed changed or not
+   if( previous-cm > 0.2){
     digitalWrite(12,1);
     start_time=millis();
-    while(millis()-start_time<=300000)    //checks the temperature inside car for next 300 seconds.
-    {
+      
+    //checks the temperature inside car for next 300 seconds.
+    while( (millis() - start_time) <= 300000){
       DHT.read11(DHT11_PIN);
       Serial.println(DHT.temperature);    //for testing purpose
-      if(DHT.temperature>=33)
-      {
+      
+       if(DHT.temperature>=33){
         digitalWrite(12,1);   //can be configured to perform an action.
         sound_alarm();
-        break;
-      }
+        break; }
+       
       delay(2000);
-    }
+     }
    }
-   previous=cm;
    
+   previous=cm;
 }
+
 
 long microsecondsToCentimeters(long microseconds) 
 {
    return microseconds / 29 / 2;
 }
 
+
 void loop()
 {
   digitalWrite(13,0);
-  check();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);
+  x=AcX;
+  y=AcY;
+  z=AcZ;  
+  AcX=Wire.read()<<8|Wire.read();  
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+    
+  
+  Serial.print("Accelerometer: ");          //for testing purpose
+  Serial.print("X = "); Serial.print(x-AcX);
+  Serial.print(" | Y = "); Serial.print(y-AcY);
+  Serial.print(" | Z = "); Serial.println(z-AcZ); 
+  
+  if( x-AcX > 3000 || y-AcY > 3000 || z-AcZ > 3000 || x-AcX <- 3000 || y-AcY <- 3000 || z-AcZ <- 3000 ) //detects vibration
+    digitalWrite(13,1);
+  else{
+    digitalWrite(13,0);
+    check();
+  }
+  delay(333);
 }
